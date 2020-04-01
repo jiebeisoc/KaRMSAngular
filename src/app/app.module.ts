1066b70dca5945a4ef6afe09a4ff4190a065b918
jeibeisoc <jiebei@comp.nus.edu.sg>
2020-04-01 16:24:06 +0800

    add template component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { AccessRightErrorComponent } from './access-right-error/access-right-error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainMenuComponent,
+    SidebarComponent,
+    AccessRightErrorComponent
   ],
   imports: [
     BrowserModule,
